6a62f8112022ead6246f1351d24919231dea0bc3
johnzheng <johnzheng2016@163.com>
2018-06-13 22:43:56 +0800

    add banner

--- app/src/main/java/com/xzxx/decorate/o2o/consumer/HomeFragment.java
+++ app/src/main/java/com/xzxx/decorate/o2o/consumer/HomeFragment.java
@@ -1,18 +1,80 @@
 package com.xzxx.decorate.o2o.consumer;
 
 import android.os.Bundle;
+import android.os.Handler;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v4.widget.SwipeRefreshLayout;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AbsListView;
+import android.widget.AdapterView;
+import com.xzxx.decorate.o2o.loader.GlideImageLoader;
+import com.youth.banner.Banner;
+import com.youth.banner.BannerConfig;
+import com.youth.banner.listener.OnBannerListener;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
-public class HomeFragment extends Fragment {
+public class HomeFragment extends Fragment implements SwipeRefreshLayout.OnRefreshListener, AdapterView
+        .OnItemClickListener, OnBannerListener {
+
+    private Banner banner;
+    private static final int REFRESH_COMPLETE = 0X1112;
+
+    private Handler mHandler = new Handler() {
+        public void handleMessage(android.os.Message msg) {
+            switch (msg.what) {
+                case REFRESH_COMPLETE:
+                    String[] urls = getResources().getStringArray(R.array.url4);
+                    List list = Arrays.asList(urls);
+                    List arrayList = new ArrayList(list);
+                    banner.update(arrayList);
+                    break;
+            }
+        }
+    };
 
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_home, null);
+        banner = (Banner) view.findViewById(R.id.home_banner);
+        banner.setImages(MyApplication.images)
+                .setImageLoader(new GlideImageLoader())
+                .setOnBannerListener(this)
+                .start();
+        banner.updateBannerStyle(BannerConfig.CIRCLE_INDICATOR);
 
-        return inflater.inflate(R.layout.fragment1,null);
+        return view;
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        banner.startAutoPlay();
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+        banner.stopAutoPlay();
+    }
+
+    @Override
+    public void onRefresh() {
+        mHandler.sendEmptyMessageDelayed(REFRESH_COMPLETE, 2000);
+    }
+
+    @Override
+    public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+
+    }
+
+    @Override
+    public void OnBannerClick(int position) {
+
     }
 }
--- app/src/main/java/com/xzxx/decorate/o2o/consumer/MyApplication.java
+++ app/src/main/java/com/xzxx/decorate/o2o/consumer/MyApplication.java
@@ -1,10 +1,19 @@
 package com.xzxx.decorate.o2o.consumer;
 
 import android.app.Application;
+import android.content.Context;
+import android.util.DisplayMetrics;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 public class MyApplication extends Application {
 
     private static MyApplication application;
+
+    public static List<?> images=new ArrayList<>();
+    public static int H,W;
 
     public static MyApplication getInstance() {
          return application;
@@ -14,5 +23,15 @@ public class MyApplication extends Application {
     public void onCreate() {
         super.onCreate();
         application = this;
+        getScreen(this);
+        String[] urls = getResources().getStringArray(R.array.url);
+        List list = Arrays.asList(urls);
+        images = new ArrayList(list);
+    }
+
+    public void getScreen(Context context) {
+        DisplayMetrics dm = context.getResources().getDisplayMetrics();
+        H=dm.heightPixels;
+        W=dm.widthPixels;
     }
 }
